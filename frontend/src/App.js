7cee43b5919234780b4d7e007b5a4fad946f9068
Rahul Arya <rahularya@berkeley.edu>
2022-01-09 04:10:48 -0800

    basic bootstrap landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import graphql from "babel-plugin-relay/macro";
 import { useLazyLoadQuery } from "react-relay";
+import Container from "react-bootstrap/Container";
 
 export default function App(): React.Node {
   const data = useLazyLoadQuery(
@@ -23,9 +24,27 @@ export default function App(): React.Node {
     { ids: ["1", "2"] }
   );
   return (
-    <div className="App">
-      {data.nodes[0].id}
-      User: {data.user?.age}
-    </div>
+    <Container className="py-4">
+      <header class="pb-3 mb-4 border-bottom">
+        <a
+          href="/"
+          class="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <span class="fs-4">CS 170 Online Judge</span>
+        </a>
+      </header>
+      <div class="p-5 mb-4 bg-light rounded-3">
+        <Container className="py-5" fluid>
+          <h1 class="display-5 fw-bold">Welcome to the CS 170 Online Judge!</h1>
+          <p class="col-md-8 fs-4">
+            This is some placeholder text to be replaced later! Probably the TAs
+            will have opinions on what to put here?
+          </p>
+          <button class="btn btn-primary btn-lg" type="button">
+            Log In
+          </button>
+        </Container>
+      </div>
+    </Container>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { RelayEnvironmentProvider } from "react-relay";
 import RelayEnvironment from "./RelayEnvironment";
+import { BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <Suspense fallback="Loading...">
-        <App />
-      </Suspense>
+      <BrowserRouter>
+        <Suspense fallback="Loading...">
+          <App />
+        </Suspense>
+      </BrowserRouter>{" "}
     </RelayEnvironmentProvider>
   </React.StrictMode>,
   document.getElementById("root")
