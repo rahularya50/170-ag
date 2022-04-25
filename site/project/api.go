67dcf8d54941260bac3f93ae798b61be5c24fa83
Rahul Arya <rahularya@berkeley.edu>
2022-04-24 18:49:39 -0700

    Keep best scores for each test case
    ghstack-source-id: a1006ee46dea7e39184f0ac49c5024f0bb75f8c1
    Pull Request resolved: https://github.com/rahularya50/170-ag/pull/12

--- site/project/api.go
+++ site/project/api.go
@@ -55,15 +55,12 @@ func (s *ProjectScoresServer) RecordSubmission(ctx context.Context, submission *
 	}
 	defer tx.Rollback()
 
-	_, err = tx.ProjectTeam.Delete().Where(projectteam.TeamID(submission.TeamId)).Exec(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	team, err := tx.ProjectTeam.Create().
 		SetTeamID(submission.TeamId).
 		SetName(submission.TeamName).
-		Save(ctx)
+		OnConflictColumns(projectteam.FieldName).
+		UpdateNewValues().
+		ID(ctx)
 	if err != nil {
 		if _, ok := err.(*ent.ConstraintError); ok {
 			return &schemas.SubmissionReply{Status: schemas.Status_DUPLICATE_TEAM_NAME}, nil
@@ -71,21 +68,50 @@ func (s *ProjectScoresServer) RecordSubmission(ctx context.Context, submission *
 		return nil, err
 	}
 
+	old_scores, err := tx.ProjectTeam.Query().
+		Where(projectteam.ID(team)).
+		QueryScores().
+		All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	score_lookup := make(map[caseKey]float64)
+	for _, score := range old_scores {
+		score_lookup[caseKey{CaseID: score.CaseID, CaseType: score.Type}] = score.Score
+	}
+
 	scores := make([]*ent.ProjectScoreCreate, len(submission.Score))
 	for i, score := range submission.Score {
+		caseType := projectscore.Type(strings.ToLower(score.CaseType.String()))
+		if err := projectscore.TypeValidator(caseType); err != nil {
+			return nil, err
+		}
+		key := caseKey{CaseID: score.CaseId, CaseType: caseType}
+		best_score, ok := score_lookup[key]
+		if !ok || best_score > score.Score {
+			best_score = score.Score
+		}
 		scores[i] = tx.ProjectScore.Create().
-			SetTeam(team).
+			SetTeamID(team).
 			SetCaseID(score.CaseId).
-			SetType(projectscore.Type(strings.ToLower(score.CaseType.String()))).
-			SetScore(score.Score)
+			SetType(caseType).
+			SetScore(best_score)
+	}
+
+	if len(scores) > 0 {
+		_, err = tx.ProjectScore.
+			Delete().
+			Where(projectscore.HasTeamWith(projectteam.ID(team))).
+			Exec(ctx)
 		if err != nil {
 			return nil, err
 		}
-	}
 
-	_, err = tx.ProjectScore.CreateBulk(scores...).Save(ctx)
-	if err != nil {
-		return nil, err
+		err = tx.ProjectScore.CreateBulk(scores...).Exec(ctx)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	err = tx.Commit()
